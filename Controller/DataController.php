acf3f85fa08e193c5e7e87e8a4012ef42ed2646a
Carlos Mendoza <inhack20@gmail.com>
2016-04-04 18:57:21 -0430

    Actualizando api

---
+++ Controller/DataController.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the TecnoReady Solutions C.A. package.
+ * 
+ * (c) www.tecnoready.com
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tecnocreaciones\Vzla\ApiBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+/**
+ * Description of DataController
+ *
+ * @author Carlos Mendoza <inhack20@gmail.com>
+ */
+class DataController extends FOSRestController
+{
+    public function getCitiesByParishAction($parishId)
+    {    
+//        $parishId = $request->get("parishId");
+        $em = $this->getDoctrine()->getManager();
+        $data = $em->getRepository("Tecnocreaciones\Vzla\EntityBundle\Entity\City")->findCitiesByParish($parishId);
+//        var_dump($data);
+        $view = $this->view($data);
+        $view->setFormat("json");
+        $view->getSerializationContext()->setGroups(array('list'));
+        return $this->handleView($view);
+    }
+    
+    public function getParishesByMunicipalityAction($municipalityId)
+    {    
+        $em = $this->getDoctrine()->getManager();
+        $states = $em->getRepository("Tecnocreaciones\Vzla\EntityBundle\Entity\Parish")->findByMunicipality($municipalityId);
+        $view = $this->view($states);
+        $view->setFormat("json");
+        $view->getSerializationContext()->setGroups(array('list'));
+        return $this->handleView($view);
+    }
+    
+    public function getUrbanizationByCityAction($cityId)
+    {    
+        $em = $this->getDoctrine()->getManager();
+        $data = $em->getRepository("Tecnocreaciones\Vzla\EntityBundle\Entity\Urbanization")->getUrbanizationByCity($cityId);
+        $view = $this->view($data);
+        $view->setFormat("json");
+        $view->getSerializationContext()->setGroups(array('list'));
+        return $this->handleView($view);
+    }
+}
